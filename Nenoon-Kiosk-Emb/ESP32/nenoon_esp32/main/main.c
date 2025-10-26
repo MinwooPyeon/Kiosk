b7284308be1e86df88a14dd35ea35489e68537d8
박주현 <yeochi1201@gmail.com>
2025-10-26 22:30:55 +0900

    feat : ESP32 - RTOS 포팅

--- Nenoon-Kiosk-Emb/ESP32/nenoon_esp32/main/main.c
+++ Nenoon-Kiosk-Emb/ESP32/nenoon_esp32/main/main.c
@@ -1,5 +1,12 @@
-#include <stdio.h>
+// app_main.c
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
 
-int main(){
-	printf("Hello World");
+void app_main(void) {
+    while (1) {
+        ESP_LOGI("MAIN", "tick");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
