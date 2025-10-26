8bb9fffd93890d862ca577a2a793fca4565b0187
rhflffkaksl <rhflffkaksl@gmail.com>
2025-10-26 20:26:56 +0900

    feat : 공통 UI 컴포넌트 추가 및 StringProvider에 getStringComposable() 메서드 추가 - 상단바, 뒤로가기 버튼, 기본 버튼 등 - StringProvider Composable 지원하도록 변경 및 프리뷰 추가

---
+++ Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/core/ui/HowToButton.kt
@@ -0,0 +1,43 @@
+package com.pixelro.nenoonkiosk.core.ui
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.pixelro.nenoonkiosk.R
+import com.pixelro.nenoonkiosk.core.util.StringProvider
+import com.pixelro.nenoonkiosk.ui.theme.NenoonKioskTheme
+import com.pixelro.nenoonkiosk.ui.theme.defaultFont
+import com.pixelro.nenoonkiosk.ui.theme.inputTextStyle
+
+/**
+ * 검사 단계 가이드 보기 버튼 컴포넌트
+ *
+ * @param onClick 클릭 이벤트
+ */
+@Composable
+fun HowToButton(
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+    ) {
+        Text(
+            text = StringProvider.getStringComposable(R.string.common_view_test_guide),
+            fontFamily = defaultFont,
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HowToButtonPreview() {
+    NenoonKioskTheme {
+        HowToButton(onClick = {})
+    }
+}
