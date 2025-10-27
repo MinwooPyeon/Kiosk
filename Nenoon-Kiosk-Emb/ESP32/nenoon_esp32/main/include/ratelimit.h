3b363e2ed27786cc4b0fe2cebd7014bac39f650c
박주현 <yeochi1201@gmail.com>
2025-10-28 00:17:31 +0900

    feat: ratelimit 헤더파일 작성

--- Nenoon-Kiosk-Emb/ESP32/nenoon_esp32/main/include/ratelimit.h
+++ Nenoon-Kiosk-Emb/ESP32/nenoon_esp32/main/include/ratelimit.h
@@ -8,8 +8,34 @@
 #ifndef MAIN_INCLUDE_RATELIMIT_H_
 #define MAIN_INCLUDE_RATELIMIT_H_
 
+#include <stdint.h>
+#include <stdbool.h>
 
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
 
+typedef struct ratelimit_s ratelimit_t;
 
+typedef struct{
+	uint32_t 	capacity;
+	uint32_t 	refill_tokens;
+	uint32_t 	period_ms;
+	bool		start_full;
+}ratelimit_config_t;
+
+/*=====static life cycle=====*/
+size_t 		ratelimit_static_size(void);
+esp_err_t	ratelimit_init(ratelimit_t* rl, const ratelimit_config_t* cfg);
+void		ratelimit_deinit(ratelimit_t* rl);
+
+/*=====dynamic life cycle=====*/
+esp_err_t	ratelimit_create(const ratelimit_config_t* cfg, ratelimit_t** out);
+void		ratelimit_destroy(ratelimit_t* rl);
+
+/*=====function=====*/
+esp_err_t ratelimit_take(ratelimit_t* rl, uint32_t n, TickType_t timeout);
+esp_err_t ratelimit_get_available(ratelimit_t* rl, uint32_t* out_tokens);
+esp_err_t ratelimit_reset(ratelimit_t* rl, bool to_full);
+esp_err_t ratelimit_reconfigure(ratelimit_t* rl, const ratelimit_config_t* cfg);
 
 #endif /* MAIN_INCLUDE_RATELIMIT_H_ */
