2fa0e6b620904b4ef95caad12ea5a2bf76d1f958
DESKTOP-RBNO6LS\SSAFY <gunbam715@gmail.com>
2025-10-27 15:15:59 +0900

    refacter: 공통 다이얼로그로 변경
    - 언어와 혈압계 다이얼로그를 공통 다이얼로그로 변경
    - 함수를 import 문 사용하도록 변경
    - 세로형 가로형 모두 Preview생성

--- Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/feature/setting/SettingsScreen.kt
+++ Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/feature/setting/SettingsScreen.kt
@@ -3,6 +3,7 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -12,12 +13,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.pixelro.nenoonkiosk.R
+import com.pixelro.nenoonkiosk.core.manager.SharedPreferencesManager
 import com.pixelro.nenoonkiosk.core.ui.TopBarVertical
 import com.pixelro.nenoonkiosk.feature.auth.login.LoginViewModel
 import com.pixelro.nenoonkiosk.feature.main.NenoonViewModel
-import com.pixelro.nenoonkiosk.feature.setting.component.BloodPressureMonitorSelector
-import com.pixelro.nenoonkiosk.feature.setting.component.LanguageSelectDialog
 import com.pixelro.nenoonkiosk.feature.setting.component.SettingItem
+import com.pixelro.nenoonkiosk.feature.setting.component.SettingSelectionDialog
+import com.pixelro.nenoonkiosk.ui.theme.NenoonKioskTheme
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
@@ -35,20 +37,47 @@ fun SettingsScreen(
 
     when (settingsDialogState) {
         NenoonViewModel.SettingsDialogState.Language -> {
-            LanguageSelectDialog(
-                updateLanguage = { viewModel.updateLanguage(it) },
-                onDismissRequest = { viewModel.setSettingsDialogState(NenoonViewModel.SettingsDialogState.None) }
+            SettingSelectionDialog(
+                titleResId = R.string.settings_language,
+                items = listOf(
+                    "ko" to "한국어",
+                    "en" to "English",
+                    "zh" to "汉语",
+                    "ja" to "日本語",
+                    "fr" to "Français",
+                    "ru" to "Русский",
+                    "es" to "Español"
+                ),
+                onItemSelected = { langCode ->
+                    viewModel.updateLanguage(langCode)
+                },
+                onDismissRequest = {
+                    viewModel.setSettingsDialogState(NenoonViewModel.SettingsDialogState.None)
+                }
             )
         }
 
         NenoonViewModel.SettingsDialogState.BloodPressureMonitorType -> {
-            BloodPressureMonitorSelector {
-                viewModel.setSettingsDialogState(NenoonViewModel.SettingsDialogState.None)
-            }
+            SettingSelectionDialog(
+                titleResId = R.string.blood_pressure_monitor_image_content_description,
+                items = listOf(
+                    "BPBIO320" to "BPBIO320",
+                    "BP170B" to "BP170B"
+                ),
+                onItemSelected = { type ->
+                    SharedPreferencesManager.putBloodPressureMonitorType(
+                        SharedPreferencesManager.BloodPressureMonitorType.valueOf(type)
+                    )
+                },
+                onDismissRequest = {
+                    viewModel.setSettingsDialogState(NenoonViewModel.SettingsDialogState.None)
+                }
+            )
         }
 
         else -> {}
     }
+
 
     SettingsScreenContent(
         isSignedIn = isSignedIn,
@@ -115,18 +144,44 @@ private fun SettingsScreenContent(
         )
     }
 }
-
 @Preview(
-    name = "설정 화면 프리뷰",
+    name = "설정 화면 세로형 프리뷰",
     showBackground = true,
     widthDp = 800,
     heightDp = 1280,
     apiLevel = 34
 )
 @Composable
-private fun SettingsScreenPreview() {
-    com.pixelro.nenoonkiosk.ui.theme.NenoonKioskTheme {
-        androidx.compose.material3.Surface(
+private fun SettingsScreenPortraitPreview() {
+    NenoonKioskTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = androidx.compose.material3.MaterialTheme.colorScheme.background
+        ) {
+            SettingsScreenContent(
+                isSignedIn = true,
+                isLocationSignedIn = true,
+                isUserSignedIn = true,
+                onLanguageClick = {},
+                onBloodPressureClick = {},
+                onLoginClick = {},
+                onBack = {}
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "설정 화면 가로형 프리뷰",
+    showBackground = true,
+    widthDp = 1280,
+    heightDp = 800,
+    apiLevel = 34
+)
+@Composable
+private fun SettingsScreenLandscapePreview() {
+    NenoonKioskTheme {
+        Surface(
             modifier = Modifier.fillMaxSize(),
             color = androidx.compose.material3.MaterialTheme.colorScheme.background
         ) {
