d2af39c34dbd49877173e6984978210c0e9f5511
박주현 <yeochi1201@gmail.com>
2025-10-28 10:42:25 +0900

    style: rename auth_adapter -> session_mgr

---
+++ Nenoon-Kiosk-Emb/ESP32/nenoon_esp32/main/include/session_mgr.h
@@ -0,0 +1,17 @@
+/*
+ * auth_adapter.h
+ *
+ *  Created on: 2025. 10. 26.
+ *      Author: SSAFY
+ */
+
+#ifndef MAIN_INCLUDE_SESSION_MGR_H_
+#define MAIN_INCLUDE_SESSION_MGR_H_
+
+#include <stdint.h>
+#include <
+
+
+
+
+#endif /* MAIN_INCLUDE_SESSION_MGR_H_ */
