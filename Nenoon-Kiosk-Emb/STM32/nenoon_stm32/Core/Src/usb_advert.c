6aeaf701233e7547c1868a0f52bfac4d9b2fa4a9
박주현 <yeochi1201@gmail.com>
2025-10-30 13:54:55 +0900

    feat: Advertise USB Scan Function

--- Nenoon-Kiosk-Emb/STM32/nenoon_stm32/Core/Src/usb_advert.c
+++ Nenoon-Kiosk-Emb/STM32/nenoon_stm32/Core/Src/usb_advert.c
@@ -42,8 +42,55 @@ const char* usb_advert_strerr(usb_advert_err_t err)
 void USB_Advert_Init(void){
 	s_count = 0;
 	s_scanned = false;
+
+	STLINK_UART_Println(TAG "Init");
 }
-usb_advert_err_t USB_Advert_Scan(void);
+usb_advert_err_t USB_Advert_Scan(void){
+	FRESULT res;
+	DIR		dir;
+	FILINFO	fno;
+
+	STLINK_UART_Println(TAG "Scan Start");
+
+	res = f_mount(&USBH_fatfs, (TCHAR const*)USBHPath, 0);
+	if(res != FR_OK){
+		return USB_ADVERT_ERR_NOT_MOUNTED;
+	}
+
+	res = f_opendir(&dir, "/");
+	if(res != FR_OK){
+		return USB_ADVERT_ERR_OPENDIR;
+	}
+
+	s_count = 0;
+	while(1){
+		res = f_readdir(&dir, &fno)
+		if(res != FR_OK){
+			f_closedir(&dir);
+			return USB_ADVERT_ERR_IO;
+		}
+		if(fno.fname[0] == 0) break;
+
+		if(!(frno.fattrib & AM_DIR)){
+			if(strstr(fno.name, ".jpg") || strstr(fno.name, ".png")){
+				strncpy(s_files[s_count], fno.fname, USB_ADVERT_MAX_NAME-1);
+				s_files[s_count][USB_ADVERT_MAX_NAME-1]='\0';
+				s_count++;
+				if(s_count >= USB_ADVERT_MAX_FILES) break;
+			}
+		}
+	}
+	f_closedir(&dir);
+
+	char msg[64];
+	snprintf(msg, sizeof(msg), TAG " found %lu files", (unsigned long)s_count);
+	STLINK_UART_PrintLn(msg);
+
+
+	s_scanned = true;
+	if(s_count == 0) return USB_ADVERT_ERR_NO_FILES;
+	return USB_ADVERT_OK;
+}
 usb_advert_err_t USB_Advert_ReadByName(const char* filename);
 usb_advert_err_t USB_Advert_ReadByIndex(uint32_t index);
 uint32_t USB_Advert_GetFileCount(void);
