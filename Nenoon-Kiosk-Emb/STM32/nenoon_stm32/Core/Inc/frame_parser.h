b516aeabb1556c6ebb8a8c5df6d8d978f51b70d7
박주현 <yeochi1201@gmail.com>
2025-11-02 22:09:36 +0900

    feat: frame_parser header file

---
+++ Nenoon-Kiosk-Emb/STM32/nenoon_stm32/Core/Inc/frame_parser.h
@@ -0,0 +1,36 @@
+/*
+ * frame_parser.h
+ *
+ *  Created on: Nov 2, 2025
+ *      Author: SSAFY
+ */
+
+#ifndef INC_FRAME_PARSER_H_
+#define INC_FRAME_PARSER_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include "frame.h"
+
+typedef void (*frame_rx_b_t)(const uint8_t * frame, size_t len);
+
+typedef enum{
+	FP_SYNC1 =0,
+	FP_SYNC2,
+	FP_HDR,
+	FP_PAYLOAD,
+	FP_CRC
+}frame_parser_t;
+
+typedef struct{
+	frame_parser_t 	state;
+	uint8_t			buf[FRAME_MAX_WIRE];
+	size_t			pos;
+	uint16_t		expect_payload;
+	frame_rx_b_t 	on_frame;
+};
+
+void frame_parser_init(frame_parser_t* p, frame_rx_cb_t cb);
+void frame_parser_feed(frame_parser_t* p, const unit8_t* data, size_t len);
+
+#endif /* INC_FRAME_PARSER_H_ */
---
+++ Nenoon-Kiosk-Emb/STM32/nenoon_stm32/Core/Src/frame_parser.c
@@ -0,0 +1,8 @@
+/*
+ * frame_parser.c
+ *
+ *  Created on: Nov 2, 2025
+ *      Author: SSAFY
+ */
+
+
