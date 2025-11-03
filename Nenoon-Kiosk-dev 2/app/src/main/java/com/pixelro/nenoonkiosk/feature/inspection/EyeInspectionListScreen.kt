1d48c800c9f007d4007709ff1f9b6460b7c8e353
오인성 <superohinsung@naver.com>
2025-11-03 16:31:42 +0900

    refactor: 파일명 변경

--- Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/feature/inspection/EyeInspectionListScreen.kt
+++ Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/feature/inspection/EyeInspectionListScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -34,7 +33,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
