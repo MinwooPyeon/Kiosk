d60372e6a08ac0907bfcca5816abab328ff8fd2c
김수현 <rhflffkaksl@gmail.com>
2025-11-03 13:45:45 +0900

    refactor : 코드 간결하게 수정

--- Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/core/util/OrientationUtil.kt
+++ Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/core/util/OrientationUtil.kt
@@ -5,6 +5,4 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
 
 @Composable
-fun isLandscape(): Boolean {
-    return LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
-}
+fun isLandscape() = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
