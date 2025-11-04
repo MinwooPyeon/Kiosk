92937341317b3fa5bb2441319ca8839c406fd643
박주현 <yeochi1201@gmail.com>
2025-11-05 00:07:40 +0900

    feat: ESP32 UART Task Create

---
+++ Nenoon-Kiosk-Emb/STM32/nenoon_stm32/Core/Inc/task_uart.h
@@ -0,0 +1,18 @@
+/*
+ * task_uart.h
+ *
+ *  Created on: Nov 4, 2025
+ *      Author: SSAFY
+ */
+
+#ifndef INC_TASK_UART_H_
+#define INC_TASK_UART_H_
+
+#define "cmsis_os.h"
+
+void task_uart_start(uint32_t stack, osPriority_t prio);
+
+
+
+
+#endif /* INC_TASK_UART_H_ */
