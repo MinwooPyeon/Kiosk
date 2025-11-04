5de68160961cd361bae78631e38c8a8e6ae8669f
박주현 <yeochi1201@gmail.com>
2025-11-05 00:38:14 +0900

    feat: media scan task create

---
+++ Nenoon-Kiosk-Emb/STM32/nenoon_stm32/Core/Inc/task_scan.h
@@ -0,0 +1,15 @@
+/*
+ * task_scan.h
+ *
+ *  Created on: Nov 5, 2025
+ *      Author: SSAFY
+ */
+
+#ifndef INC_TASK_SCAN_H_
+#define INC_TASK_SCAN_H_
+
+#include "cmsis_os2.h"
+
+void task_scan_start(uint32_t stack, osPriority_t prio);
+
+#endif /* INC_TASK_SCAN_H_ */
