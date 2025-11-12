5170fd0f10d4fbc6f847c0a133a867d2487df5b1
천지윤 <gunbam715@gmail.com>
2025-11-12 14:12:20 +0900

    chore: 주석삭제

--- Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/feature/intro/IntroScreen.kt
+++ Nenoon-Kiosk-dev 2/app/src/main/java/com/pixelro/nenoonkiosk/feature/intro/IntroScreen.kt
@@ -15,13 +15,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pixelro.nenoonkiosk.core.ui.Logo
 import com.pixelro.nenoonkiosk.core.ui.NenoonTopBar
 import com.pixelro.nenoonkiosk.core.ui.SettingsButton
@@ -76,6 +76,7 @@ fun IntroScreen(
             // 가로 모드: Row로 배치
             Row(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .align(Alignment.Center),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
